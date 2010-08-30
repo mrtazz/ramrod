51eaa61bdbfc21a187d907869afd7a47aeb464e9
Daniel Schauenberg <d@unwiredcouch.com>
2010-08-30 13:47:02 +0200

    move some general layout from index to layout

--- lib/ramrod/views/index.rb
+++ lib/ramrod/views/index.rb
@@ -2,14 +2,6 @@ class Ramrod
   module Views
     class Index < Layout
 
-      def headline
-        "Ramrod Equalizer Unit"
-      end
-
-      def byline
-        "Command multiple CI instances for equal awesomeness."
-      end
-
       def projects
         @projects
       end
--- lib/ramrod/views/layout.rb
+++ lib/ramrod/views/layout.rb
@@ -6,6 +6,14 @@ class Ramrod
         "Ramrod Will Now Take Over Navigational Control"
       end
 
+      def headline
+        "Ramrod Equalizer Unit"
+      end
+
+      def byline
+        "Command multiple CI instances for equal awesomeness."
+      end
+
     end
   end
 end
